eb026abeddef4de7521b133170294e7738686309
Nayem <nayem.alam97@gmail.com>
2023-06-07 11:02:41 +0100

    basic react set up

---
+++ tech-test/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import AllContacts from "./components/AllContacts";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Contact Manager</h1>
+      <AllContacts />
+    </div>
+  );
+}
+
+export default App;
---
+++ tech-test/src/components/AllContacts.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AllContacts = () => {
+  return (
+    <div className="all-contacts">
+      <h2>All Contacts</h2>
+      <div className="contacts-container"></div>
+    </div>
+  );
+};
+
+export default AllContacts;
