eac2198dd6356c2dc61c03296c48eb130465f32e
Nayem <nayem.alam97@gmail.com>
2023-06-09 12:34:27 +0100

    basic table llayout

--- tech-test/src/components/AllContacts.jsx
+++ tech-test/src/components/AllContacts.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import { FaEdit } from "react-icons/fa";
 
 const AllContacts = () => {
   return (
     <div className="all-contacts">
       <h2>All Contacts</h2>
-      <div className="contacts-container"></div>
+      <div className="contacts-container">
+        <table class="contacts-table">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Company</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Simohamed</td>
+              <td>Akniouene</td>
+              <td>akniouene.s@example.com</td>
+              <td>Example Design Ltd</td>
+              <td>
+                <FaEdit />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
